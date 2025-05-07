80e6385b3f5017b3dec26e0130eb3b3cbd46b04f
Svitacheva <133576425+Svitacheva@users.noreply.github.com>
2025-05-07 18:09:07 +0300

    Add files via upload

---
+++ day01/ex04/to_dictionary.py
@@ -0,0 +1,35 @@
+def to_dictionary():
+    list_of_tuples = [
+        ('Russia', '25'),
+        ('France', '132'),
+        ('Germany', '132'),
+        ('Spain', '178'),
+        ('Italy', '162'),
+        ('Portugal', '17'),
+        ('Finland', '3'),
+        ('Hungary', '2'),
+        ('The Netherlands', '28'),
+        ('The USA', '610'),
+        ('The United Kingdom', '95'),
+        ('China', '83'),
+        ('Iran', '76'),
+        ('Turkey', '65'),
+        ('Belgium', '34'),
+        ('Canada', '28'),
+        ('Switzerland', '26'),
+        ('Brazil', '25'),
+        ('Austria', '14'),
+        ('Israel', '12')
+    ]
+
+    result = {}
+    for country, number in list_of_tuples:
+        result.setdefault(number, []).append(country)
+    return result
+
+
+if __name__ == "__main__":
+    dictionary = to_dictionary()
+    for number in sorted(dictionary, key=int):
+        for country in dictionary[number]:
+            print(f" '{number}' : '{country}'")
